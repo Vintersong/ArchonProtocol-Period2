6349db2c59dc409fc354c826fe1d6bd10ad2108b
Andrei Moldovean <moldovean.i.andrei@gmail.com>
2024-12-17 19:20:28 +0100

    Double Jump Tutorial
    Added double jump tutorial and temp UI confirm button.

---
+++ Source/archonProtocol_p2/archonProtocol_p2.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "archonProtocol_p2.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, archonProtocol_p2, "archonProtocol_p2" );
